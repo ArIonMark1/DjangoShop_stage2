923dc4425bd5e1318c17a774426132489be774b5
ArIonMark1 <andreydatsenko777@gmail.com>
2021-06-20 00:18:24 +0200

    homeWork lesson7 admin-rea

--- admins/forms.py
+++ admins/forms.py
@@ -1,8 +1,23 @@
-from users.forms import UserRegistrationForm
+from users.forms import UserRegistrationForm, UserProfileForm
+from django import forms
 from users.models import User
 
 
 class UserAdminRegisterForm(UserRegistrationForm):
+    image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'custom-file-input'}), required=False)
+
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'username', 'image', 'email', 'password1', 'password2')
+
+
+class UserAdminProfileForm(UserProfileForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4', 'readonly': False}))
+
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'class': 'form-control py-4', 'readonly': False}))
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'image', 'username', 'email')
--- admins/views.py
+++ admins/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import messages
+from django.urls import reverse
+from admins.forms import UserAdminRegisterForm, UserAdminProfileForm
 
 from users.models import User
+
 
 # Create your views here.
 # CRUD
@@ -13,21 +17,49 @@ def index(request):
 
 # READ
 def admin_users(request):
-    context = {'users': User.objects.all()}
+    context = {'title': 'GeekShop - Admin | Пользователи', 'users': User.objects.all()}
     return render(request, 'admins/admin-users-read.html', context)
 
 
 # CREATE
 def admin_users_create(request):
-    context = {}
-    return render(request, 'admins/admin-users-create.html')
+    if request.method == 'POST':
+        form = UserAdminRegisterForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Регистрация прошла успешно!!')
+            return HttpResponseRedirect(reverse('admins:admin_users'))
+    else:
+        form = UserAdminRegisterForm()
+
+    context = {'title': 'GeekShop - Admin | Регистрация', 'form': form}
+    return render(request, 'admins/admin-users-create.html', context)
 
 
 # UPDATE
-def admin_users_update(request, id_user):
-    return render(request, 'admins/admin-users-update-delete.html')
+def admin_users_update(request, id_user=None):
+    selected_user = User.objects.get(id=id_user)
+    if request.method == 'POST':
+        form = UserAdminProfileForm(data=request.POST, files=request.FILES, instance=selected_user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Изменения данных пользователя "{selected_user}" успешно сохранены!!')
+            return HttpResponseRedirect(reverse('admins:admin_users'))
+    else:
+        form = UserAdminProfileForm(instance=selected_user)
+
+    context = {'title': 'GeekShop - Admin | Пользователь',
+               'form': form,
+               'selected_user': selected_user,
+               }
+    return render(request, 'admins/admin-users-update-delete.html', context)
 
 
 # DELETE
 def admin_users_delete(request, id_user):
+    user = User.objects.get(id=id_user)
+    user.is_active = False
+    user.save()
+    messages.success(request, f'Пользователь "{user}" успешно удален!!')
+    return HttpResponseRedirect(reverse('admins:admin_users'))
     pass
